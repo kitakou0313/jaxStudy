47583e973f67ed8e00e41b4b761f067f558067c6
kitakou0313 <kitakou0313@gmail.com>
2021-10-13 16:30:19 +0000

    Test autgrad

---
+++ transformations.py
@@ -0,0 +1,12 @@
+from jax import grad
+import jax.numpy as jnp
+
+def tanh(x):
+    """
+    docstring
+    """
+    y = jnp.exp(-2.0*x)
+    return (1.0-y) / (1.0+y)
+
+grad_tanh = grad(tanh)
+print(grad_tanh(1.0))
