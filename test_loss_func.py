b7031ed2a8ea6166aabffe3ab73f62d971603d26
kitakou0313 <kitakou0313@gmail.com>
2022-11-13 05:00:30 +0000

    Implement some optimization problems

--- test_loss_func.py
+++ test_loss_func.py
@@ -7,15 +7,18 @@ def loss_func(X: np.ndarray, X_trg_list: list, Y_trg:np.ndarray) -> np.ndarray:
     """
     trg_xの各行との距離にexpした値を返す
     """
-    loss_sum = 0
+    
 
-    l2_loss = np.sum(np.linalg.norm(X-Y_trg, ord=2, axis=1)**2)
-    loss_sum += l2_loss
+    trg_loss = np.sum(np.linalg.norm(X-Y_trg, ord=2, axis=1)**2)
 
+    regularization_loss = 0
     for x, X_trg in zip(X, X_trg_list):
-        loss_sum -= np.sum(np.exp(-(np.linalg.norm(X_trg - x, ord=2, axis=1)**2)))
+        regularization_loss -= np.sum(np.exp(-((np.linalg.norm(X_trg - x, ord=2, axis=1)**2))))
 
-    return loss_sum
+    regularization_loss *= 15
+    print(trg_loss, regularization_loss)
+
+    return trg_loss+regularization_loss
 
 
 def loss_func_for(x:np.ndarray, a_arr:list) -> np.ndarray:
@@ -40,17 +43,12 @@ if __name__ == "__main__":
 
     x_1_trg = np.array(
         [[1.5, 1.5, 1.5, 1.5, 1.5],
-         [1.5, 1.5, 1.5, 1.5, 1.5],
-         [1.5, 1.5, 1.5, 1.5, 1.5],
-         [1.5, 1.5, 1.5, 1.5, 1.5],
-         [1.5, 1.5, 1.5, 1.5, 1.5],
-         [1.5, 1.5, 1.5, 1.5, 1.5]], dtype=np.float32)
+         [0.5, 0.5, 0.5, 0.5, 0.5]], dtype=np.float32)
 
     x_2_trg = np.array(
-        [[1.5, 1.5, 1.5, 1.5, 1.5],
-         [1.5, 1.5, 1.5, 1.5, 1.5],
-         [1.5, 1.5, 1.5, 1.5, 1.5],
-         [1.5, 1.5, 1.5, 1.5, 1.5]], dtype=np.float32)
+        [[1.9, 1.9, 1.9, 1.9, 1.9],
+         [2.5, 2.5, 2.5, 2.5, 2.5], 
+         [3., 3., 3., 3., 3.]], dtype=np.float32)
 
 
     # print(np.linalg.norm(x_1_trg - X[0],ord=2, axis=1))
@@ -60,7 +58,7 @@ if __name__ == "__main__":
 
     Y_trg = np.array(
         [[3.,3.,3.,3.,3.],
-        [6.,6.,6.,6.,6.,]]
+        [0.,0.,0.,0.,0.,]]
     ,dtype=np.float32)
 
     grad_loss_func = jit(grad(
@@ -68,9 +66,12 @@ if __name__ == "__main__":
     ))
 
     for iteration in range(30):
+        print("Iteraion", iteration)
+        (loss_func(X, X_trg, Y_trg))
         d_X = grad_loss_func(X, X_trg, Y_trg)
         X += -0.1*d_X
-
-        print("Iteraion", iteration)
         print(X)
-        print(loss_func(X, X_trg, Y_trg))
+        print()
+
+    print("Trg vecs")
+    print(Y_trg)
